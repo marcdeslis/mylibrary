83253a4dcba00b89252444436a11ebd354d31448
marcdeslis <marc-tiratorn.deslis@dauphine.eu>
2024-09-12 11:08:07 +0200

    feat: update the code majorly
    BREAKING CHANGE: hello_world2 function removed it cannot be used

--- mylibrary/src/mylibrary/mylibrary.py
+++ mylibrary/src/mylibrary/mylibrary.py
@@ -1,7 +1,2 @@
-def hello_world():
-    "returns the string 'Hello World' " 
-    return "Hello World"
 
-def hello_world2():
-    """Returns the string 'Hello World 2'."""
-    return "Hello World 2"
+
